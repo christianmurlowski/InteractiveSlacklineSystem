c1c6bf2b2391758e53a99fbcc669b7b2969b3949
christianmurlowski <christianmurlowski@gmail.com>
2017-12-29 12:41:29 +0100

    last study

--- Assets/Scripts/SetData.cs
+++ Assets/Scripts/SetData.cs
@@ -8,8 +8,8 @@ public class SetData : MonoBehaviour {
 	{
 		// TODO Just for test purposes -> Delete in production
 		UserSelectionManager.TestSetCurrentUser();
-		PlayerPrefs.SetInt("CurrentTierId", 2);
-		PlayerPrefs.SetInt("CurrentExerciseId", 3);
+		PlayerPrefs.SetInt("CurrentTierId", 1);
+		PlayerPrefs.SetInt("CurrentExerciseId", 2);
 		PlayerPrefs.SetInt("CurrentSideId", 0);
 		PlayerPrefs.SetInt("CurrentRepetitionId", 0);
 		Debug.Log("CurrentTierId: " + PlayerPrefs.GetInt("CurrentTierId"));
